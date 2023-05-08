df8bcbb81059818d1620c39adb98a532b1722126
Dimitar Ivanov <89870517+muhitos@users.noreply.github.com>
2023-05-08 19:12:04 +0300

    Add files via upload

---
+++ DIMITAR CODDING/finance_information.py
@@ -0,0 +1,17 @@
+# import yfinance as f
+#
+# company = f.Ticker('TSLA')
+# print(company.info)
+# hist = company.history(period='5d')
+
+#import yfinance as f
+# company = f.Ticker('TSLA')
+# stock_data = company.history(period='1mo')
+# print(stock_data)
+#--------------------------------------------------------------
+
+# import yfinance as f
+# import easygui
+# company = f.Ticker('TSLA')
+# easygui.msgbox(f'{company.info}', title='simple-gui')
+# или (f'{stock_data}', title='simple-gui')
